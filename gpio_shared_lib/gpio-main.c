a2be7af9705ee4a29353ebadf9c26f6a234f47e2
Stefan Pietzonke <jay-t@gmx.net>
2017-08-27 06:13:24 +0200

    Merged I2C library to GPIO library.

--- gpio_shared_lib/gpio-main.c
+++ gpio_shared_lib/gpio-main.c
@@ -33,10 +33,11 @@
 #include "../dll_shared_libs/nanovmlib.h"
 
 #include <wiringPi.h>
+#include <wiringPiI2C.h>
 
 int gpio_setup (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnum, S8 stacksize)
 {
-	wiringPiSetup ();
+	wiringPiSetupSys ();
 	return (0);
 }
 
@@ -73,7 +74,11 @@ int gpio_digital_read (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 thr
 	
 	if (stpull_l (threadnum, pthreads, stacksize, &pin) != ERR_STACK_OK) printf ("gpio_digital_read: pin stack error!\n");
 
+	printf ("gpio_digital_read: pin: %lli ... ", pin);
+	
 	value = digitalRead (pin);
+	
+	printf ("= %lli \n", value);
 	
 	if (stpush_l (threadnum, pthreads, stacksize, value) != ERR_STACK_OK) printf ("gpio_digital_read: value return stack error!\n");
 	return (0);
@@ -187,5 +192,184 @@ int gpio_pinmode (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnu
 	if (stpull_l (threadnum, pthreads, stacksize, &pin) != ERR_STACK_OK) printf ("gpio_pinmode: pin stack error!\n");
 	
 	pinMode (pin, mode);
+	return (0);
+}
+
+
+// I2C ----------------------------------------------------
+
+int i2c_setup (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnum, S8 stacksize)
+{
+	S8 id, fd;
+	S8 ret;
+
+	struct pthreads *pthreads;
+	struct varlist *varlist;
+	struct vm_mem *vm_mem;
+	
+	pthreads = (struct pthreads *) pthreads_ptr;
+	varlist = (struct varlist *) varlist_ptr;
+	vm_mem = (struct vm_mem *) vm_mem_ptr;
+
+	if (stpull_l (threadnum, pthreads, stacksize, &id) != ERR_STACK_OK) printf ("i2c_setup: id stack error!\n");
+	
+	if ((fd = wiringPiI2CSetup (id)) < 0)
+	{
+		printf ("i2c_setup: ERROR: opening I2C bus!\n");
+	}
+
+	if (stpush_l (threadnum, pthreads, stacksize, fd) != ERR_STACK_OK) printf ("i2c_setup: fd return stack error!\n");
+	return (0);
+}
+
+int i2c_read (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnum, S8 stacksize)
+{
+	S8 fd;
+	S8 ret;
+
+	struct pthreads *pthreads;
+	struct varlist *varlist;
+	struct vm_mem *vm_mem;
+	
+	pthreads = (struct pthreads *) pthreads_ptr;
+	varlist = (struct varlist *) varlist_ptr;
+	vm_mem = (struct vm_mem *) vm_mem_ptr;
+
+	if (stpull_l (threadnum, pthreads, stacksize, &fd) != ERR_STACK_OK) printf ("i2c_read: fd stack error!\n");
+	
+	ret = wiringPiI2CRead (fd);
+	if (ret < 0)
+	{
+		printf ("i2c_read: ERROR!\n");
+	}
+	
+	if (stpush_l (threadnum, pthreads, stacksize, ret) != ERR_STACK_OK) printf ("i2c_read: ret return stack error!\n");
+	return (0);
+}
+
+int i2c_write (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnum, S8 stacksize)
+{
+	S8 fd;
+	S8 data, ret;
+
+	struct pthreads *pthreads;
+	struct varlist *varlist;
+	struct vm_mem *vm_mem;
+	
+	pthreads = (struct pthreads *) pthreads_ptr;
+	varlist = (struct varlist *) varlist_ptr;
+	vm_mem = (struct vm_mem *) vm_mem_ptr;
+
+	if (stpull_l (threadnum, pthreads, stacksize, &data) != ERR_STACK_OK) printf ("i2c_write: data stack error!\n");
+	if (stpull_l (threadnum, pthreads, stacksize, &fd) != ERR_STACK_OK) printf ("i2c_write: fd stack error!\n");
+	
+	ret = wiringPiI2CWrite (fd, data);
+	if (ret < 0)
+	{
+		printf ("i2c_write: ERROR!\n");
+	}
+	return (0);
+}
+
+int i2c_writereg8 (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnum, S8 stacksize)
+{
+	S8 fd;
+	S8 data, reg, ret;
+
+	struct pthreads *pthreads;
+	struct varlist *varlist;
+	struct vm_mem *vm_mem;
+	
+	pthreads = (struct pthreads *) pthreads_ptr;
+	varlist = (struct varlist *) varlist_ptr;
+	vm_mem = (struct vm_mem *) vm_mem_ptr;
+
+	if (stpull_l (threadnum, pthreads, stacksize, &data) != ERR_STACK_OK) printf ("i2c_writereg8: data stack error!\n");
+	if (stpull_l (threadnum, pthreads, stacksize, &reg) != ERR_STACK_OK) printf ("i2c_writereg8: reg stack error!\n");
+	if (stpull_l (threadnum, pthreads, stacksize, &fd) != ERR_STACK_OK) printf ("i2c_writereg8: fd stack error!\n");
+	
+	ret = wiringPiI2CWriteReg8 (fd, reg, data);
+	if (ret < 0)
+	{
+		printf ("i2c_writereg8: ERROR!\n");
+	}
+	return (0);
+}
+
+int i2c_writereg16 (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnum, S8 stacksize)
+{
+	S8 fd;
+	S8 data, reg, ret;
+
+	struct pthreads *pthreads;
+	struct varlist *varlist;
+	struct vm_mem *vm_mem;
+	
+	pthreads = (struct pthreads *) pthreads_ptr;
+	varlist = (struct varlist *) varlist_ptr;
+	vm_mem = (struct vm_mem *) vm_mem_ptr;
+
+	if (stpull_l (threadnum, pthreads, stacksize, &data) != ERR_STACK_OK) printf ("i2c_writereg16: data stack error!\n");
+	if (stpull_l (threadnum, pthreads, stacksize, &reg) != ERR_STACK_OK) printf ("i2c_writereg16: reg stack error!\n");
+	if (stpull_l (threadnum, pthreads, stacksize, &fd) != ERR_STACK_OK) printf ("i2c_writereg16: fd stack error!\n");
+	
+	ret = wiringPiI2CWriteReg16 (fd, reg, data);
+	if (ret < 0)
+	{
+		printf ("i2c_writereg16: ERROR!\n");
+	}
+	return (0);
+}
+
+
+int i2c_readreg8 (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnum, S8 stacksize)
+{
+	S8 fd;
+	S8 reg, ret;
+
+	struct pthreads *pthreads;
+	struct varlist *varlist;
+	struct vm_mem *vm_mem;
+	
+	pthreads = (struct pthreads *) pthreads_ptr;
+	varlist = (struct varlist *) varlist_ptr;
+	vm_mem = (struct vm_mem *) vm_mem_ptr;
+
+	if (stpull_l (threadnum, pthreads, stacksize, &reg) != ERR_STACK_OK) printf ("i2c_readreg8: reg stack error!\n");
+	if (stpull_l (threadnum, pthreads, stacksize, &fd) != ERR_STACK_OK) printf ("i2c_readreg8: fd stack error!\n");
+	
+	ret = wiringPiI2CReadReg8 (fd, reg);
+	if (ret < 0)
+	{
+		printf ("i2c_readreg8: ERROR!\n");
+	}
+	
+	if (stpush_l (threadnum, pthreads, stacksize, ret) != ERR_STACK_OK) printf ("i2c_readreg8: ret return stack error!\n");
+	return (0);
+}
+
+int i2c_readreg16 (U1 *pthreads_ptr, U1 *varlist_ptr, U1 *vm_mem_ptr, S4 threadnum, S8 stacksize)
+{
+	S8 fd;
+	S8 reg, ret;
+
+	struct pthreads *pthreads;
+	struct varlist *varlist;
+	struct vm_mem *vm_mem;
+	
+	pthreads = (struct pthreads *) pthreads_ptr;
+	varlist = (struct varlist *) varlist_ptr;
+	vm_mem = (struct vm_mem *) vm_mem_ptr;
+
+	if (stpull_l (threadnum, pthreads, stacksize, &reg) != ERR_STACK_OK) printf ("i2c_readreg16: reg stack error!\n");
+	if (stpull_l (threadnum, pthreads, stacksize, &fd) != ERR_STACK_OK) printf ("i2c_readreg16: fd stack error!\n");
+	
+	ret = wiringPiI2CReadReg16 (fd, reg);
+	if (ret < 0)
+	{
+		printf ("i2c_readreg16: ERROR!\n");
+	}
+	
+	if (stpush_l (threadnum, pthreads, stacksize, ret) != ERR_STACK_OK) printf ("i2c_readreg16: ret return stack error!\n");
 	return (0);
 }
